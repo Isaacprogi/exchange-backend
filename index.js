77966eda93f27e6b8613bdc6d4853fd0105ecb00
Isaac <isaaconyes80@gmail.com>
2024-04-23 06:59:53 +0100

    updated cors

--- index.js
+++ index.js
@@ -20,21 +20,30 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = ['https://exchanger-psi.vercel.app', `http://localhost:5173`];
+// const allowedOrigins = ['https://exchanger-psi.vercel.app', `http://localhost:5173`];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//        console.log(origin)
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// }
+
 
 const corsOptions = {
   origin: function (origin, callback) {
-       console.log(origin)
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
+    callback(null, true);
   },
   optionsSuccessStatus: 200,
-  credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}
+};
 
 
 app.use(cors(corsOptions));
