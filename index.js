75b2c862a74ee1a1f0541faad2b3bc38a3937e6a
Isaac <isaaconyes80@gmail.com>
2024-04-23 06:20:43 +0100

    revied cors options

--- index.js
+++ index.js
@@ -20,24 +20,24 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = ['https://bingo-samp-2.vercel.app', `http://localhost:5173`];
+// const allowedOrigins = ['https://bingo-samp-2.vercel.app', `http://localhost:5173`];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-       console.log(origin)
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200,
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//        console.log(origin)
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// }
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
